de9632166fa544b1c17e53f12b2556f96f807a1f
leblancmehdi376-tech <leblancmehdi376@gmail.com>
2026-06-28 00:07:27 +0200

    Update enemies.ts

--- lib/game/enemies.ts
+++ lib/game/enemies.ts
@@ -294,8 +294,8 @@ export function generateEnemy(wave: number, palier: number): Enemy {
   const maxHp  = Math.floor(baseHp * hpMult);
 
   const pixelCoins = isBoss
-    ? Math.floor(71  * Math.pow(1.122, global - 1))
-    : Math.floor(31  * Math.pow(1.143, global - 1));
+    ? Math.floor(71  * Math.pow(1.128, global - 1))
+    : Math.floor(31  * Math.pow(1.145, global - 1));
 
   const gemsReward = isBoss ? Math.ceil(palier / 2) : (wave === 5 ? 1 : 0);
 
